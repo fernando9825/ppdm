14ae9c117828093f4555245c7dbd3fef06831611
Fernando9825 <fer25alvarado@gmail.com>
2019-05-07 13:15:13 -0600

    updated port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const consultarProducto = require('./gestion/consultarProducto');
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 5000;
 
 // cargar productos al iniciar el servidor
 let productos = consultarProducto.getProduct();
